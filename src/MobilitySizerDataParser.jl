2eb424d158e642df1ea1b29b73e95e9bddb3cf7e
Jens Top <topjens@gmail.com>
2026-06-04 17:52:07 +0300

    This time bug is fixed for real

--- src/MobilitySizerDataParser.jl
+++ src/MobilitySizerDataParser.jl
@@ -86,7 +86,8 @@ function parse_file(path::AbstractString)
         # 𝕟ⁱⁿᵛ = rinv(𝕣.N, δ, λ₁=0.1, λ₂=1.0)
         𝕟ⁱⁿᵛ² = rinv2(𝕣.N, δ, λ₁=0.1, λ₂=1.0)
 
-        experiment[!, :𝕟ⁱⁿᵛ²] = 𝕟ⁱⁿᵛ²
+        experiment[!, :Dp_inv] = 𝕟ⁱⁿᵛ².Dp
+        experiment[!, :𝕟ⁱⁿᵛ²] = 𝕟ⁱⁿᵛ².N
 
         # p1 = scatter(experiment[!,:Dp], experiment[!,:Rcn],
         #              xscale = :log10,
